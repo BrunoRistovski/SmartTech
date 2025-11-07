4074523ba9243915dcc733d4d10a07a49c631e78
Bruno Ristovski <brunoristovski@gmail.com>
2025-11-07 18:05:55 +0100

    Updated

--- src/main/java/smart/tech/com/SmartTech/web/PaymentRestController.java
+++ src/main/java/smart/tech/com/SmartTech/web/PaymentRestController.java
@@ -3,15 +3,13 @@ package smart.tech.com.SmartTech.web;
 import com.stripe.net.StripeResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import smart.tech.com.SmartTech.model.DTO.ProductRequestDTO;
 import smart.tech.com.SmartTech.model.DTO.StripeResponseDTO;
 import smart.tech.com.SmartTech.services.impl.StripeService;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
 @RequestMapping("/api/payment")
 public class PaymentRestController {
 
